242bf1764ba403f81a98d3497ac602715a12d5e0
stubfx <14958556+stubfx@users.noreply.github.com>
2025-10-23 16:44:24 +0200

    Add n queryparam

--- globals.js
+++ globals.js
@@ -1,5 +1,10 @@
 // tunables
-export const TEX_SIDE = 1200; // agents = TEX_SIDE^2
+
+const params = new URLSearchParams(window.location.search);
+const number = params.get("n");
+console.log(number);
+
+export const TEX_SIDE = number || 1200; // agents = TEX_SIDE^2
 export let POINT_SIZE = 1; // px
 // export let DRAG = 8; // damping, not used.
 export let STEP_LEN = .8; // pos += vel * STEP_LEN
